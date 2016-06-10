63c4c4591e8749e6c98cc34cf0ed4ba6bedcdb6c
Matthew Aebersold <matt.aebersold@bkwld.com>
2016-06-10 15:36:53 -0700

    Working on the mobile nav dropdown

--- assets/application.js
+++ assets/application.js
@@ -680,5 +680,39 @@ Theme.TriggerEvents = (function($) {
   // Document ready
   $(function() {
 
+
+    $('.grabber').on('click', function(e) {
+      var cur = $(e.currentTarget);
+
+      if(!cur.hasClass('closable')) {
+        cur.addClass('closable');
+        open();
+      } else {
+        cur.removeClass('closable');
+        close();
+      }
+
+    });
+
+    var open = function() {
+      $('nav').css('top', 0);
+    };
+
+    var close = function() {
+      $('nav').css('top', '-100%');
+    };
+
+    console.log('bruh');
+    $(window).on('resize', function(e) {
+      console.log('in resize', $(window).width)
+      if($(window).width() > 768) {
+        console.log('in')
+        $('nav').css('top', 'auto');
+        return;
+      }
+    });
+
+
+
   }); // End document ready
 })(Theme.jQuery);
