6d39f44853698d170098a88e0daf3a27fc344276
matthew <mattaebersold@gmail.com>
2016-12-05 15:49:08 -0800

    Added local-storage based newsletter modal here for future use.

--- assets/application.js
+++ assets/application.js
@@ -1490,6 +1490,44 @@ Theme.jQuery(function($) {
   });
 })(Theme.jQuery);
 
+//------ scripts/controllers/newsletter.controller.js
+$(document).ready(function() {
+
+  if(localStorage.getItem('newsletterShown') == 'true') {
+    console.log('tue....');
+  } else {
+    console.log('falseeeeee')
+    localStorage.setItem('newsletterShown', 'true');
+
+    var modal = $('#newsletter-modal'),
+        close = modal.find('.modal-close'),
+        bkgd = modal.find('.bkgd'),
+        content = modal.find('.newsletter-content');
+
+    // show the modal
+    modal.addClass('show');
+
+    setTimeout(function() {
+      bkgd.addClass('show');
+      close.addClass('show');
+    }, 1000)
+
+    setTimeout(function() {
+      content.addClass('show');
+    }, 1500)
+
+    bkgd.on('click', function() {
+      modal.fadeOut(200);
+    });
+
+    close.on('click', function() {
+      modal.fadeOut(200);
+    });
+
+  }
+
+});
+
 //------ scripts/controllers/search.controller.js
 (function($) {
 
